5069bee27601123f1a5b267efb9db5d6764087ac
Alizah Manag <2019079@icacademy.org>
2017-03-08 03:27:51 +0000

    Say something descriptive

--- my-javascript.js
+++ my-javascript.js
@@ -14,4 +14,31 @@ $(function() {
     let content = prompt("Early life");
     $("#earlylife").text(content);
   });
+    var hide = "#discography"
+  
+  $("#dangerously").click(function() {
+    $("#dangerously-in-love, #DIL").show(500);
+    $("#4-album, #lemonade-album").hide(500);
+    $("#FOUR, #LEMONS").hide(500);
+  
+  });
+  
+  $("#4").click(function() {
+    $("#dangerously-in-love, #lemonade-album").hide(500);
+    $("#4-album, #FOUR").show(500);
+    $("#DIL, #LEMONS").hide(500);
+  });
+  
+  $("#lemonade").click(function() {
+    $("#dangerously-in-love, #4-album").hide(500);
+    $("#lemonade-album, #LEMONS").show(500);
+    $("#DIL, #FOUR").hide(500);
+  });
+  
+  
+  
+  
+  
+  
+  
 });
